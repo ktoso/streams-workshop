679bd8cbef8c02c858440bafa0398e98386cacdd
Will Sargent <will.sargent@gmail.com>
2014-11-07 13:00:02 -0800

    Upgrade to 0.10

--- src/exercises/exerciseFour/MovementVideo.scala
+++ src/exercises/exerciseFour/MovementVideo.scala
@@ -1,35 +1,62 @@
 package exerciseFour
 
-import akka.actor.ActorSystem
-import akka.stream.MaterializerSettings
-import video.Frame
-import org.reactivestreams.api.Producer
 import java.io.File
-import akka.stream.scaladsl.Flow
+
+import akka.actor.ActorSystem
+import akka.stream.FlowMaterializer
+import akka.stream.scaladsl._
+import akka.stream.stage.{Context, Directive, StatefulStage}
+import org.reactivestreams.{Publisher, Subscriber}
+import video.Frame
 
 object MovementVideo {
 
   /**
    * run:
-   *   ./activator 'runMain exerciseFour.MovementVideo'
+   * ./activator 'runMain exerciseFour.ManipulateVideo'
    *
    */
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
-    val settings = MaterializerSettings()
+    implicit val materializer = FlowMaterializer()
 
     // ------------
-    // EXERCISE 4.1
+    // EXERCISE 4.2
     // ------------
-    // Fill in the code necessary to create a flow dsl and manipulate the video stream to be grayscale.
-
-    // Given - The location of the mp4 we can display (note first few seconds are still frame).
-    val mp4 = new File("goose.mp4")
-
-    //Create a Producer from the file system
-    val fileProducer: Producer[Frame] = video.FFMpeg.readFile(mp4, system)
-    val flow = Flow(fileProducer)
+    // Fill in the code necessary to create a flow dsl from a screen capture and then add a a transform
+    // that will show the movement within the video, rather than the raw picture.
+    // Hint:  Use the video.frameUtil.diff method to difference two video frames.
 
     // TODO - Your code here to consume and manipulate the video stream in a flow dsl.
+
+    val videoStream: Publisher[Frame] = video.FFMpeg.readFile(new File("goose.mp4"), system)
+    val source = Source(videoStream)
+    val videoSubscriber: Subscriber[Frame] = video.Display.create(system)
+    val sink = Sink(videoSubscriber)
+
+    val transformer = new StatefulStage[Frame, Frame] {
+      var last: Frame = _
+
+      val pullFirstFrame = new State {
+        override def onPush(elem: Frame, ctx: Context[Frame]): Directive = {
+          last = elem
+          become(diffFrames)
+          ctx.pull()
+        }
+      }
+
+      val diffFrames = new State {
+        override def onPush(f: Frame, ctx: Context[Frame]): Directive = {
+          val diffed: Frame = video.frameUtil.diff(last, f)
+          last = f
+          ctx.push(diffed)
+        }
+      }
+
+
+      override def initial = pullFirstFrame
+    }
+
+    source.transform[Frame](() => transformer).runWith(sink)
   }
 }
