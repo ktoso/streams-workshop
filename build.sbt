fa823444aced527d1125fd5ae75e35e755734a20
Konrad Malawski <konrad.malawski@project13.pl>
2015-01-18 00:08:53 +0100

    WIP latest

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ version := "1.0"
 scalaVersion := "2.11.4"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-stream-experimental" % "0.10" withSources(),
+  "com.typesafe.akka" %% "akka-stream-experimental" % "1.0-M2" withSources(),
   "com.typesafe.akka" %% "akka-testkit" % "2.3.6",
   "com.typesafe.akka" %% "akka-cluster" % "2.3.6",
   "org.boofcv" % "xuggler" % "0.17",
--- src/exercises/exerciseFour/MovementVideo.scala
+++ src/exercises/exerciseFour/MovementVideo.scala
@@ -3,16 +3,19 @@ package exerciseFour
 import java.io.File
 
 import akka.actor.ActorSystem
+import akka.stream.stage.{Directive, Context, StageState, StatefulStage}
+import akka.stream.{FlowMaterializer, Transformer}
 import akka.stream.scaladsl._
-import akka.stream.{FlowMaterializer, MaterializerSettings}
 import org.reactivestreams.{Publisher, Subscriber}
 import video.Frame
+
+import scala.collection.immutable.Seq
 
 object MovementVideo {
 
   /**
    * run:
-   *   ./activator 'runMain exerciseFour.MovementVideo'
+   * ./activator 'runMain exerciseFour.ManipulateVideo'
    *
    */
   def main(args: Array[String]): Unit = {
@@ -20,21 +23,42 @@ object MovementVideo {
     implicit val materializer = FlowMaterializer()
 
     // ------------
-    // EXERCISE 4.1
+    // EXERCISE 4.2
     // ------------
-    // Fill in the code necessary to create a flow dsl and manipulate the video stream to be grayscale.
+    // Fill in the code necessary to create a flow dsl from a screen capture and then add a a transform
+    // that will show the movement within the video, rather than the raw picture.
+    // Hint:  Use the video.frameUtil.diff method to difference two video frames.
 
-    // Given - The location of the mp4 we can display (note first few seconds are still frame).
-    val mp4 = new File("goose.mp4")
+    // TODO - Your code here to consume and manipulate the video stream in a flow dsl.
 
-    //Create a Producer from the file system
-    val filePublisher: Publisher[Frame] = video.FFMpeg.readFile(mp4, system)
-    val source = Source(filePublisher)
+    val videoStream: Publisher[Frame] = video.FFMpeg.readFile(new File("goose.mp4"), system)
+    val source = Source(videoStream)
     val videoSubscriber: Subscriber[Frame] = video.Display.create(system)
     val sink = Sink(videoSubscriber)
 
-    source.map { frame =>
-      video.frameUtil.grayscale(frame)
-    }.runWith(sink)
+    val transformer = new StatefulStage[Frame, Frame] {
+      var last: Frame = _
+
+      val pullFirstFrame = new State {
+        override def onPush(elem: Frame, ctx: Context[Frame]): Directive = {
+          last = elem
+          become(diffFrames)
+          ctx.pull()
+        }
+      }
+
+      val diffFrames = new State {
+        override def onPush(f: Frame, ctx: Context[Frame]): Directive = {
+          val diffed: Frame = video.frameUtil.diff(last, f)
+          last = f
+          ctx.push(diffed)
+        }
+      }
+
+
+      override def initial = pullFirstFrame
+    }
+
+    source.transform[Frame](() => transformer).runWith(sink)
   }
 }
